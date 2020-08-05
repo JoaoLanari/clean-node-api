71f653a10ce52d6e38211aae237d412191af06f7
Joao Lanari dos Mares Guia <joaolanari@yahoo.com.br>
2020-08-05 12:41:21 -0300

    feat: ensure BcryptAdapter return a hash on success

--- src/infra/criptografy/bcrypt-adapter.spec.ts
+++ src/infra/criptografy/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('rash'))
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcryot with correct values', async () => {
     const salt = 12
@@ -8,5 +14,12 @@ describe('Bcrypt Adapter', () => {
     const hashSppy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSppy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a rah o success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const rash = await sut.encrypt('any_value')
+    expect(rash).toBe('rash')
   })
 })
