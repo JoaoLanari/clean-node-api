2c85df510b3969640f92771c1e7b108e069b2d9c
Joao Lanari dos Mares Guia <joaolanari@yahoo.com.br>
2020-08-07 10:09:33 -0300

    feat: ensure AccountMongoRepository returns account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,6 +1,6 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
 
-export const mongoHelper = {
+export const MongoHelper = {
   client: null as MongoClient,
 
   async connect (url: string): Promise<void> {
@@ -9,7 +9,12 @@ export const mongoHelper = {
       useUnifiedTopology: true
     })
   },
+
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
