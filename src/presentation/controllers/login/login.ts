086f401f5d67b280b8da22fc498a4c08234d7176
João Lanari <joao@rarolabs.com.br>
2020-09-07 20:27:33 -0300

    refactor: group LoginController dependencies in a sigle file

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,6 @@
-import { Controller, HttpRequest, HttpRespose } from '../../protocols'
+import { Controller, HttpRequest, HttpRespose, EmailValidator, Authentication } from './login-protocols'
 import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
 import { MissingParamError, InvalidParamError } from '../../erros'
-import { EmailValidator } from '../signup/signup-protocols'
-import { Authentication } from '../../../domain/usecases/authentication'
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
