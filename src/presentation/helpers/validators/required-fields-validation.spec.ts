df0806f2eae965973b41a8a91da9b435ff214d71
João Lanari <joao@rarolabs.com.br>
2020-09-07 23:17:14 -0300

    test: ensure RequiredFiledValidation not return if validation succed

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -3,8 +3,14 @@ import { MissingParamError } from '../../erros'
 
 describe('RequiredFiled Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('filed')
+    const sut = new RequiredFieldValidation('field')
     const error = sut.validate({ name: 'any_name' })
-    expect(error).toEqual(new MissingParamError('filed'))
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not return if validation succeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
   })
 })
