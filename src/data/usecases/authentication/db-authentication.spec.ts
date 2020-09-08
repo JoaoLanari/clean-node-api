7695cf9ba45b30a66a87d2ca665dc490e5b75bca
João Lanari <joao@rarolabs.com.br>
2020-09-08 09:23:54 -0300

    test: ensure DbAuthentication throw if TokenGenerator throws

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -117,4 +117,11 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(makeFakeAuthentication())
     expect(generateSpy).toHaveBeenCalledWith('any_id')
   })
+
+  test('Should throw if TokenGenerator throws', async () => {
+    const { sut, tokenGeneratorStub } = makeSut()
+    jest.spyOn(tokenGeneratorStub, 'generate').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promisse = sut.auth(makeFakeAuthentication())
+    await expect(promisse).rejects.toThrow()
+  })
 })
